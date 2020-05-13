7a13f2be8ae6d9cb07fe4ba285b1a0e88f53deb2
Rafael Lage Tavares <rafael.tavares@xing.com>
2020-05-13 11:33:52 +0100

    remove main image from home

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,19 +1,15 @@
 import React from 'react'
-import Img from 'gatsby-image'
 
 import styles from './hero.module.css'
 
-export default ({ data }) => (
+export default ({ data }) => {
+  console.log(data);
+  return (
   <div className={styles.hero}>
-    <Img
-      className={styles.heroImage}
-      alt={data.name}
-      fluid={data.heroImage.fluid}
-    />
     <div className={styles.heroDetails}>
       <h3 className={styles.heroHeadline}>{data.name}</h3>
       <p className={styles.heroTitle}>{data.title}</p>
       <p>{data.shortBio.shortBio}</p>
     </div>
   </div>
-)
+)}
